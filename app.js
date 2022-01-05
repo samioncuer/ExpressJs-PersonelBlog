a32298962fa036e86cff1c3699e8b707a1ad877b
samioncuer95 <sami.oncuer2@gmail.com>
2022-01-05 19:01:35 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log("Function working");
-};
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = 3000;
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
